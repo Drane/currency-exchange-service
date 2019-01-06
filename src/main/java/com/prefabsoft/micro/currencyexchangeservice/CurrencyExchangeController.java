be9001bd40436d83472dd97285547d23d397eedd
Jochen.Szostek <jochen.szostek@economie.fgov.be>
2019-01-06 19:17:18 +0100

    added sleuth, busy adding zipkin

--- src/main/java/com/prefabsoft/micro/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/prefabsoft/micro/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,15 +1,15 @@
 package com.prefabsoft.micro.currencyexchangeservice;
 
 import com.prefabsoft.micro.currencyexchangeservice.bean.ExchangeValue;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-
 @RestController
+@Slf4j
 public class CurrencyExchangeController {
 
     @Autowired
@@ -19,9 +19,10 @@ public class CurrencyExchangeController {
     private ExchangeValueRepository repository;
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
-    public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
+    public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
         ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
         exchangeValue.setPort(getPort());
+        log.info("exchangeValue: {}", exchangeValue);
         return exchangeValue;
     }
 
